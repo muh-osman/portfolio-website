484f3088e65974809e2e81a7b299497dffd35582
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-06-22 00:53:48 +0300

    Fix

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,41 @@
+import {motion} from 'framer-motion'
+import { footerVariants, staggerChildren } from "./utils/motion";
+
+
 export default function Footer() {
   return (
-    <footer>
-        Footer goes here
-    </footer>
+    <motion.footer
+    variants={staggerChildren}
+    initial="hidden"
+    whileInView="show"
+    viewport={{ once: true, amount: 0.25 }}
+    
+    >
+
+        <motion.div
+          variants={footerVariants}
+           className="footer-slogan">
+          <h1>Let's make something<br/>amazing together.</h1>
+          <h1>Start by <a href="mailto:me@mail.com">Saying Hi</a></h1>
+        </motion.div>
+
+        <motion.div
+          variants={footerVariants}
+          >
+          <div>
+            <h2>Information</h2>
+            <a href="https://goo.gl/maps/33ZCPRbhHaMXVMYt5">145 New York, FL 5467, USA</a>
+          </div>
+
+          <div className="footer-link">
+            <a href="#services">Services</a>
+            <a href="#portfolio">Works</a>
+            <a href="#testimonials">Notes</a>
+            <a href="#experience">Experience</a>
+          </div>
+
+        </motion.div>
+
+    </motion.footer>
   )
 }
--- src/Services .js
+++ src/Services .js
@@ -2,6 +2,9 @@
 import computer from "./assets/image/computer-logo.svg"
 import phone from "./assets/image/phone-logo.svg"
 import brand from "./assets/image/brand-logo.svg"
+// Framer motion npm
+import {motion} from 'framer-motion'
+import {fadeIn, staggerContainer, textVariant} from './utils/motion'
 
 
 export default function Services() {
@@ -13,55 +16,68 @@ export default function Services() {
 
 
   return (
-    <section id="services">
-      <div className="left-services">
-          <div>
+    <motion.section
+    variants={staggerContainer}
+    initial="hidden"
+    whileInView="show"
+    viewport={{ once: true, amount: 0.25 }}
+    
+    id="services"
+    >
+
+      <motion.div
+
+      className="left-services">
+          <motion.div variants = {fadeIn("right", "tween", (1)*0.2, 1)}>
             <div className="image-box-one">
               <img src={computer} alt="computer logo" />
             </div>
-            <div>
+            <div className="services-title">
               <h2>Website Design</h2>
               <h3>76 Projects</h3>
             </div>
-          </div>
+          </motion.div>
 
-          <div>
+          <motion.div variants = {fadeIn("right", "tween", (2)*0.2, 1)}>
             <div className="image-box-two">
               <img src={phone} alt="phone logo" />
             </div>
-            <div>
+            <div className="services-title">
               <h2>Mobile App Design</h2>
               <h3>63 Projects</h3>
             </div>
-          </div>
+          </motion.div>
 
-          <div>
+          <motion.div variants = {fadeIn("right", "tween", (3)*0.2, 1)}>
             <div className="image-box-three">
               <img src={brand} alt="brand logo" />
             </div>
-            <div>
+            <div className="services-title">
               <h2>Brand Identity</h2>
               <h3>47 Projects</h3>
             </div>
-          </div>
-      </div>
+          </motion.div>
+      </motion.div>
 
 
-      <div className="right-services">
-        <h1>What do I help?</h1>
-        <p>I will help you with finging a solution and solve your problem, We use process design to create digital products. Besides that also help their business.</p>
-        <p>We use process design to create digital products. Besides that also help their business</p>
-        <div>
+      <motion.div
+      variants={textVariant(0.5)}
+      className="right-services">
+          <h1>What do I help?</h1>
+          <p>I will help you with finging a solution and solve your problem, We use process design to create digital products. Besides that also help their business.</p>
+          <p>We use process design to create digital products. Besides that also help their business</p>
           <div>
-            <h2>+285</h2>
-            <h3>Projects Completed</h3>
+            <div>
+              <h2>+285</h2>
+              <h3>Projects Completed</h3>
+            </div>
+            <div>
+              <h2>+190</h2>
+              <h3>Happy Clients</h3>
+            </div>
           </div>
-          <div>
-            <h2>+190</h2>
-            <h3>Happy Clients</h3>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+      </motion.div>
+
+    </motion.section>
+  )
 }
